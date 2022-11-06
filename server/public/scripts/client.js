ec4afc804b018569c67262433732f54a3c3237de
Tracey <tracey@Traceys-Air.home>
2022-11-06 11:21:43 -0600

    Calculator working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,26 +1,99 @@
 $(document).ready(onReady);
 
-let currentOperation = "";
+let currentOperation = '';
+let currentNumber = '';
+let expressionToSend = {
+    num1: '',
+    num2: '',
+    operator: '',
+};
 
 function onReady() {
-
     $('.number').on('click', addNumber);
     $('.operator').on('click', setOperation);
     $('#equals').on('click', evaluateExpression);
 }
 
 function addNumber() {
+    // Add to current number
+    currentNumber += $(this).text();
+    if (expressionToSend.operator == ''){
+        console.log('adding to num1');
+        expressionToSend.num1 = currentNumber;
+    } else {
+        console.log('adding to num2');
+        expressionToSend.num2 = currentNumber;
+    }
+    $('input').val(currentNumber);
+    console.log(currentNumber);
 
 }
 
 function setOperation() {
+    if (expressionToSend.num1 !== '' && expressionToSend.num2 !== '' && expressionToSend.operator !== ''){
+        // if all fields are already filled out, then evaluate expression, 
+        // reset expressionToSend, set num1 to result of evaluated expression,
+        // and set operator to the selected operator.
+        //$('input').val('');
+        currentNumber = '';
+        evaluateExpression();
+        expressionToSend.operator = $(this).text();
+    } else if (expressionToSend.num1 !== '' && expressionToSend.operator === ''){
+        // Set operator
+        //$('input').val('');
+        currentNumber = '';
+        expressionToSend.operator = $(this).text();
+    } else {
+        //alert('Something is wrong with setting the operation');
 
+    }
+    console.log('num1', expressionToSend.num1);
+    console.log('num2', expressionToSend.num2);
+    console.log('operator', expressionToSend.operator);
+}
+
+function getEvaluatedExpression(){
+    $.ajax({
+        method: 'GET',
+        url: '/expression'
+    }).then(function(response) {
+        // reset expressionToSend 
+        expressionToSend.num2 = '';
+        expressionToSend.operator = '';
+        // set num1 to result of evaluated expression
+        expressionToSend.num1 = response.num;
+        // display answer on calculator screen
+        $('input').val(response.num);
+        // Call render
+        render(response);
+    }).catch(function(error){
+        alert('getEvaluatedExpression failure', error);
+    });
 }
 
 function evaluateExpression() {
+    // check if all fields are filled in
+    if (expressionToSend.num1 !== '' && expressionToSend.num2 !== '' && expressionToSend.operator !== ''){
+        // POST the expression to the server.
+        $.ajax({
+            method: 'POST',
+            url: '/expression', 
+            data: expressionToSend,
+          }).then(function(response) {
+              // GET the evaluated expression from the server.
+              getEvaluatedExpression();
+          }).catch(function(response) {
+              alert('evaluateExpression failed');
+          });
+    }
 
 }
 
-function render() {
-    
+function render(response) {
+    // Render to DOM.
+    $('#history').append(`
+        <li class="answer">
+            ${response.answerString}
+        </li>
+    `);
 }
--- server/server.js
+++ server/server.js
@@ -8,4 +8,51 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('server/public'));
 
-let expression = [];
+let answer = {
+    answerString: '',
+    num: 0,
+};
+
+// GET for /expression
+app.get('/expression', (req, res) => {
+    // send the answer to the client
+    res.send(answer);
+});
+
+app.post('/expression', (req, res) => {
+    console.log('in post expression');
+    let expression = req.body;
+    // properties of expression:
+    // num1, num2, operator
+
+    // set answer to blank
+    answer.answerString = '';
+    answer.num = '';
+
+    // evaluate the expression
+    console.log('the operator',expression.operator);
+    if (expression.operator === '+'){
+        console.log('adding');
+        answer.num = Number(expression.num1) + Number(expression.num2);
+    } else if (expression.operator === '-'){
+        console.log('subtracting');
+        answer.num = Number(expression.num1) - Number(expression.num2);
+    } else if (expression.operator === '*'){ 
+        console.log('multiplying');
+        answer.num = Number(expression.num1) * Number(expression.num2);
+    } else if (expression.operator === '/'){
+        console.log('dividing');
+        answer.num = Number(expression.num1) / Number(expression.num2);
+    } else {
+        console.log('Error with calculation - this shouldn\'t happen');
+    }
+    answer.answerString = expression.num1 + ' ' + expression.operator + ' ' + expression.num2 + ' = ' + answer.num;
+    res.sendStatus(201);
+});
+
+
+//The port where this is listening from. 
+app.listen(PORT, () => {
+    console.log('Server is running on port', PORT)
+  })
+  
